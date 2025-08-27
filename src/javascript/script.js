6da7636ffae965e962535d822c6c1e4f6ac2cccd
eduardo.melo010@gmail.com <seu@email.com>
2025-08-27 18:24:20 -0300

    atualizações

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", async (e) => {
     document.getElementById("player-info").style.display = "none";
 
     try {
-        const response = await fetch(`https://api.balldontlie.io/v1/players?search=${playerName}`, {
+        const response = await fetch(`https://api.balldontlie.io/v1/players?search=${encodeURIComponent(playerName)}`, {
             headers: { "Authorization": API_KEY }
         });
 
